aabb1b2b2a119485bf40d75acf59b7c76221a3d8
blezza8-ops <blezza8@icloud.com>
2026-08-14 18:39:13 +0400

    Delete Склад directory

--- Склад/js/index.js
+++
@@ -1,9 +0,0 @@
-import { actionAddBtn } from "./addForm.js";
-import { actionEditBtn } from "./editForm.js";
-import { navigate } from "./navigate.js";
-
-document.addEventListener('DOMContentLoaded', function() {
-    navigate();
-    actionEditBtn();
-    actionAddBtn();
-})
